4577e0a3b62ae77cee3ef0d9ed409993b4f79562
overstarry <jm1179356055@gmail.com>
2022-01-21 23:13:16 +0800

    add example model and policy file

--- casbin.go
+++ casbin.go
@@ -3,12 +3,13 @@ package casbin
 import (
 	"context"
 	"fmt"
+	"github.com/casbin/casbin/v2"
 	"log"
 
 	"github.com/go-kratos/kratos/v2/middleware"
 )
 
-func AuthMiddleware(logger log.Logger) middleware.Middleware {
+func AuthMiddleware(e *casbin.Enforcer) middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			log.Println("auth middleware in", req)
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/overstarry/kratos-casbin
 
 go 1.17
 
-require github.com/go-kratos/kratos/v2 v2.1.4
+require (
+	github.com/casbin/casbin/v2 v2.40.6
+	github.com/go-kratos/kratos/v2 v2.1.4
+)
+
+require github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
